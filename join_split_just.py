aa5eb4e75d1109ce44a7cab91344dadd4c4725ca
John C <84040540+johnchd@users.noreply.github.com>
2022-12-31 14:00:10 -0500

    Add files via upload

---
+++ join_split_just.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 20:10:57 2022
+
+@author: john
+"""
+
+
+     
+#join information from list
+dragonLoot = ['gold coin', 'test','w000oooo0o','gold','BTC', 'ruby']
+
+print(", ".join(dragonLoot))
+print()
+print(" - ".join(dragonLoot))
+print()
+print("\n".join(dragonLoot))
+
+print()
+
+print('Hello'.center(20, '-'))
+print('Hello'.rjust(20, '-'))
+print('Hello'.ljust(20, '-'))
