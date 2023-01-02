a77a7c7bffba96dddee3489a05befcf6aa69bf69
John C <84040540+johnchd@users.noreply.github.com>
2023-01-02 18:52:07 -0500

    Add files via upload

---
+++ simpleScripts/setDef-Count.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 12:35:14 2022
+
+@author: john
+"""
+
+message = 'aaaaaaaaaaa ffffffffddddddddd ooooooo'
+count = {}
+
+
+for chars in message:
+    count.setdefault(chars, 0)
+    count[chars] = count[chars] + 1
+    
+print(count)
+
+
+
