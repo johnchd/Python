ce8ebd93501dd62c22a376c79e0ae966d15c26dc
John C <84040540+johnchd@users.noreply.github.com>
2023-01-15 19:52:49 -0500

    Update selectiveCopy.py

--- Useful-scripts/selectiveCopy.py
+++ Useful-scripts/selectiveCopy.py
@@ -3,7 +3,8 @@
 import os, glob, shutil
 
 #Selective Copy
-#Write a program that walks through a folder tree and searches for files 
+
+#Program that walks through a folder tree and searches for files 
 #with a certain file extension (such as .pdf or .jpg). 
 #Copy these files from whatever location they are in to a new folder.
 
