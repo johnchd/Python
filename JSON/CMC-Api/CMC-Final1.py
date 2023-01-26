3c12c5ef5bb0d5d83cf88108f85887d1aa49302d
John C <84040540+johnchd@users.noreply.github.com>
2023-01-26 08:42:09 -0500

    Add files via upload

--- JSON/CMC-Api/CMC-Final1.py
+++ JSON/CMC-Api/CMC-Final1.py
@@ -5,23 +5,21 @@ from requests.auth import HTTPBasicAuth
 import urllib.request
 import os
 
-os.chdir("/Users/john/Desktop/GoogleDrive/ATBS/WebScraping/jsonParse/Generated-Files")
+# os.chdir("/Users/john/Desktop/<path>")
 
 
 #Auth to CMC's API
 #Part 1 - Hit CMC API, get response, and place into dict (parseResponse)
 #Part 2 - Put symbols into list (First 2 (btc, eth))
 #Part 3 - Insert the Symbols from list into URL
-#Part 4 - Go to each url within (urlList), put seperate responses into 1 list
-#Part 5
-
+#Part 4 - Get each response / insert into variable, then parse
 
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=2&convert=USD'
 
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '<API-KEY>',
+  'X-CMC_PRO_API_KEY': '<api-key>',
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -47,75 +45,30 @@ print('Part 2 --- List of symbols:\n' + str(nameList))
 #Part 3 - Insert Symbols from list into URL
 #https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol={}
 
-'''
 print()
-print('Part 3 - List of SYMBOLS appended to URL: ')
-for symbol in nameList:
-    urlList = []
-    urlSymbol = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol={}'.format(symbol)
-    urlList.append(urlSymbol)
-    #print(urlList)
-
-    
-    for url in urlList:
-        req = requests.get(url, headers=headers)
-        #this gets the ALL the contents of a response
-        allResponses = req.content
-        parseResponse1 = json.loads(allResponses)
-        readAll = json.dumps(parseResponse1)
-        print(readAll)
-'''
-
-print()
-#print('Part 3 - List of SYMBOLS appended to URL: ')
+print('Part 3 - List of SYMBOLS appended to URL: ')          
 test1 = [] 
 for symbol in nameList:
     urlList = []
     urlSymbol = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol={}'.format(symbol)
     urlList.append(urlSymbol)
-    #print(urlList)
+    print(urlList)
 
-    #part 4 - get each response and put it into a variable... 
+    #Part 4 - Get each response / insert into variable, then parse
     for url in urlList:
         req = requests.get(url, headers=headers)
         #print(req)
         allResponses = req.content
-        #print(allResponses)
-        test1.append(allResponses)
-print(test1[0])
-        #print(type(test))
-        #for i in test:
-            #print(i)
+        parseResponse = json.loads(allResponses)
+        readAll = json.dumps(parseResponse)
+        test1.append(readAll)
+#print(test1)
 
-
-
-
-
-        #for response in req:
-            #test = []
-            #response = req.content
-            #print(response)
-            #test.append(response)
-            #print(response)
-            #test.append(allResponses)
-
-        
-        #print(test)
-        #this gets the ALL the contents of a response
-        #allResponses = req.content
-        #print(allResponses)
-           
-        
-        
-            #test.append(i)
-        
-        
- 
-        
-    
-    
-
-
+for response in test1:
+    #print(response) #works
+    test1_to_dict = json.loads(response)
+    #print(test1_to_dict) #verify response can be printed
+    print(list(test1_to_dict['data'].values())[0]['id'])
 
 
 
